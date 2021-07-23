61813c68b827d07faaa4e4ee0849963ec2a4c7c4
Nadia Tumareva <tumarevanadya@gmail.com>
2021-07-23 14:15:46 -0400

    Validation added

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,9 +1,8 @@
+from posts.models import Comment, Follow, Group, Post, User
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueTogetherValidator
-
-from posts.models import Comment, Follow, Group, Post, User
 
 
 class FollowSerializer(serializers.ModelSerializer):
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,11 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from posts.models import Follow, Group, Post, User
 from rest_framework import filters, mixins, viewsets
 from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
-from posts.models import Follow, Group, Post, User
 
 from .permissions import OwnerOrReadOnly
 from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
